16216287aec757980e5345d96fca3470e6ef58d7
Ruslan04071996 <ruslan040796@mail.ru>
2020-09-09 18:17:21 +0300

    Реализовал функционал debounce

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDom from "react-dom";
 import { Input } from "antd";
 import { Card } from "antd";
-function App() {
-  return (
-    <div>
-      <Input placeholder="Type to search..." />
-      <Card title="Hi" />
-    </div>
-  );
+import {DebounceInput} from 'react-debounce-input';
+import MovieSearch from "./components/movie-search";
+class App extends Component {
+  state = {
+    date: [],
+    value: ''
+  };
+  createItem(id, title, date, genre, desk, stars, rate, poster) {
+    return {
+      id,
+      title,
+      date,
+      genre,
+      desk,
+      stars,
+      rate,
+      poster,
+    };
+  }
+  onChangeHandler = (e) => {
+    this.setState({value: e.target.value})
+    new MovieSearch().getMovie(this.state.value)
+  }
+  render() {
+    return (
+      <div>
+        <DebounceInput
+          minLength={2}
+          debounceTimeout={400}
+          onChange={this.onChangeHandler} />
+ 
+        <p>Value: {this.state.value}</p>
+      </div>
+    );
+  }
 }
 ReactDom.render(<App />, document.querySelector("#root"));
-
