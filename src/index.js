37b5650fd2f73a18c2ea6d64548aa1eede0ef625
Ruslan04071996 <ruslan040796@mail.ru>
2020-09-14 16:42:37 +0300

    Рефакторинг кода

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React, { Component } from "react";
 import ReactDom from "react-dom";
-import Card from './components/card';
 import CardList from './components/cardList';
 import TabPanel from './components/tabPanel';
 import { DebounceInput } from "react-debounce-input";
 import MovieSearch from "./components/movie-search";
-import { Rate } from "antd";
+import Loader from './components/loader';
 import "./index.css";
 import "antd/dist/antd.css";
-import { Tabs } from "antd";
-const { TabPane } = Tabs;
+
 class App extends Component {
   state = {
     data: [],
@@ -33,9 +31,7 @@ class App extends Component {
     if (value) {
       new MovieSearch().getMovie(value).then((body) => {
         const needArr = body.results;
-        console.log(needArr);
         const newData = needArr.map((item) => {
-          console.log(typeof item.release_date);
           return this.createItem(
             item.id,
             item.original_title,
@@ -59,14 +55,6 @@ class App extends Component {
    
     return (
       <div className="main">
-        {/* <Tabs centered={true} defaultActiveKey="2">
-          <TabPane tab={<span>Tab 1</span>} key="1">
-            Tab 1
-          </TabPane>
-          <TabPane tab={<span>Tab 2</span>} key="2">
-            Tab 2
-          </TabPane>
-        </Tabs> */}
         <TabPanel />
         <DebounceInput
           minLength={1}
