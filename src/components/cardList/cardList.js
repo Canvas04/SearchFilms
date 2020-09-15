ffabd1dd49473557eafe2916a14358dc25d1a4a6
Ruslan04071996 <ruslan040796@mail.ru>
2020-09-15 17:16:55 +0300

    УЛучшил верстку работающего лоадера

--- src/components/cardList/cardList.js
+++ src/components/cardList/cardList.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Card from "../card";
 import Loader from "../loader";
-
+import "./cardList.css";
 function CardList({ data, loading }) {
   const loader = loading ? <Loader /> : null;
   const cardEl = !loading ? <Card data={data} loading={loading} /> : null;
-  
+  let classNames = "list-content";
+  if (loading) {
+    classNames += " loader";
+  }
   return (
-    <ul className="list-content">
+    <ul className={classNames}>
       {loader}
       {cardEl}
       {/* <Card data={data} loading={loading}/> */}
