d4dfe4d1baf4879306d9b25f64c6320a102901ab
Ruslan04071996 <ruslan040796@mail.ru>
2020-09-22 16:33:30 +0300

    Начинаю редактировать панель таба

--- src/components/tabPanel/tabPanel.js
+++ src/components/tabPanel/tabPanel.js
@@ -1,17 +1,17 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import { Tabs } from "antd";
 const { TabPane } = Tabs;
 export default function TabPanel () {
     return (
-        <Fragment >
-             <Tabs centered={true} defaultActiveKey="2">
+        < >
+             <Tabs centered={true} defaultActiveKey="1">
           <TabPane tab={<span>Tab 1</span>} key="1">
-            Tab 1
+            
           </TabPane>
           <TabPane tab={<span>Tab 2</span>} key="2">
-            Tab 2
+            
           </TabPane>
         </Tabs>
-        </Fragment>
+        </>
     )
 }
